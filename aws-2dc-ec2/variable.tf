6b2f00ad4c52e8af5082244d08824b7d9d30d254
Takuto Suzuki <tasuzuki@confluent.io>
2023-08-25 07:50:29 +0900

    change to a non private ip

--- aws-2dc-ec2/variable.tf
+++ aws-2dc-ec2/variable.tf
@@ -6,17 +6,17 @@ variable "name" {
 
 # CIDR Block of Jumphost in DC1. Must not overlap with ec2_vpc_cidr_block
 variable "jumphost_vpc_cidr_block" {
-  default = "172.22.0.0/16"
+  default = "179.22.0.0/16"
 }
 
 # CIDR Block of VPC in DC1. Must be a subset of CIDR in dc1_zones
 variable "dc1_vpc_cidr_block" {
-  default = "172.16.0.0/16"
+  default = "179.16.0.0/16"
 }
 
 # CIDR Block of VPC in DC2. Must be a subset of CIDR in dc2_zones
 variable "dc2_vpc_cidr_block" {
-  default = "172.17.0.0/16"
+  default = "179.18.0.0/16"
 }
 
 # Name of the SSH key to be setup in DC1 instances.
@@ -54,34 +54,34 @@ variable "dc1_zones" {
     private_subnets = {
       private-1a = {
         name = "private-1a"
-        cidr = "172.16.10.0/24"
+        cidr = "179.16.10.0/24"
         az   = "ap-northeast-1a"
       },
       private-1c = {
         name = "private-1c"
-        cidr = "172.16.11.0/24"
+        cidr = "179.16.11.0/24"
         az   = "ap-northeast-1c"
       },
       private-1d = {
         name = "private-1d"
-        cidr = "172.16.12.0/24"
+        cidr = "179.16.12.0/24"
         az   = "ap-northeast-1d"
       }
     }
     public_subnets = {
       public-1a = {
         name = "public-1a"
-        cidr = "172.16.13.0/24"
+        cidr = "179.16.13.0/24"
         az   = "ap-northeast-1a"
       },
       public-1c = {
         name = "public-1c"
-        cidr = "172.16.14.0/24"
+        cidr = "179.16.14.0/24"
         az   = "ap-northeast-1c"
       },
       public-1d = {
         name = "public-1d"
-        cidr = "172.16.15.0/24"
+        cidr = "179.16.15.0/24"
         az   = "ap-northeast-1d"
       }
     }
@@ -96,34 +96,34 @@ variable "dc2_zones" {
     private_subnets = {
       private-3a = {
         name = "private-3a"
-        cidr = "172.17.20.0/24"
+        cidr = "179.18.20.0/24"
         az   = "ap-northeast-3a"
       },
       private-3b = {
         name = "private-3b"
-        cidr = "172.17.21.0/24"
+        cidr = "179.18.21.0/24"
         az   = "ap-northeast-3b"
       },
       private-3c = {
         name = "private-3c"
-        cidr = "172.17.22.0/24"
+        cidr = "179.18.22.0/24"
         az   = "ap-northeast-3c"
       }
     }
     public_subnets = {
       public-3a = {
         name = "public-3a"
-        cidr = "172.17.23.0/24"
+        cidr = "179.18.23.0/24"
         az   = "ap-northeast-3a"
       },
       public-3b = {
         name = "public-3b"
-        cidr = "172.17.24.0/24"
+        cidr = "179.18.24.0/24"
         az   = "ap-northeast-3b"
       },
       public-3c = {
         name = "public-3c"
-        cidr = "172.17.25.0/24"
+        cidr = "179.18.25.0/24"
         az   = "ap-northeast-3c"
       }
     }
